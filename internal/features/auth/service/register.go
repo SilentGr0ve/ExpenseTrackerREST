b0e31624a9e6b5e3dbc357ccfcdd96e7b33abd21
Egor <egor.04.ru04@mail.ru>
2026-06-25 19:11:59 +0400

    feature/auth: jwt.go returned

--- internal/features/auth/service/register.go
+++ internal/features/auth/service/register.go
@@ -37,30 +37,3 @@ func (s *AuthService) Register(ctx context.Context, req domain.RegisterRequest) 
 
 	return user, nil
 }
-
-func (s *AuthService) generateAccessToken(ID uuid.UUID, email string) (string, error) {
-	claims := jwt.MapClaims{
-		"sub":   ID,
-		"email": email,
-		"exp":   time.Now().Add(s.jwtExpiry).Unix(),
-		"iat":   time.Now().Unix(),
-		"iss":   "expense-tracker",
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	signedToken, err := token.SignedString([]byte(s.jwtSecret))
-	if err != nil {
-		return "", err
-	}
-
-	return signedToken, nil
-}
-
-func hashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
-	if err != nil {
-		return "", err
-	}
-
-	return string(bytes), nil
-}
